dfac2bfb3a5ed7ee4c65cc864c3a0118b6e1ac45
hj0702 <parting321@naver.com>
2025-06-01 06:37:13 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,14 +5,17 @@ SHARE_DIR=/share/kocom
 mkdir -p $SHARE_DIR
 
 if [ ! -f $SHARE_DIR/kocom.conf ]; then
-	mv /kocom.conf $SHARE_DIR
+	cp /kocom.conf $SHARE_DIR/
 fi
 
-mv /kocom.py $SHARE_DIR
+cp /kocom.py $SHARE_DIR/
 
 echo "[Info] Run Kocom Wallpad with RS485!"
-cd $SHARE_DIR
-python3 $SHARE_DIR/kocom.py
 
-# dev용 루프 (컨테이너 유지)
+cd $SHARE_DIR
+
+# python 실행 실패하더라도 컨테이너 유지
+python3 kocom.py || echo "[ERROR] Python script exited with error code $?"
+
+# 컨테이너 유지용 루프
 while true; do echo "still live"; sleep 100; done
